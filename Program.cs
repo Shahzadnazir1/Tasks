641a991ed67914cd4d87d1e3722cc5aabce24cb2
Shahzadnazir1 <shahzadnazir2021@gmail.com>
2025-04-16 08:48:19 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int studentAge; 
+        studentAge = 20; 
+
+        
+
+        string studentName = "John Doe";
+        double CGPA = 3.1;
+        
+
+        
+        Console.WriteLine("Student Name: " + studentName);
+        Console.WriteLine("Student Age: " + studentAge);
+        Console.WriteLine("CGPA+ cgpa");
+       
+    }
+}
